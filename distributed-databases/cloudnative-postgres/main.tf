05cb1939956bb2b67415dd12c8e9d54973083d75
Andrew Stangl <andrewstangl@gmail.com>
2023-10-04 10:24:25 +0200

    Revert to destroy cluster

--- distributed-databases/cloudnative-postgres/main.tf
+++ distributed-databases/cloudnative-postgres/main.tf
@@ -42,17 +42,17 @@ module "eks" {
       type        = "ingress"
       self        = true
     }
-    # # Allows Control Plane Nodes to talk to Worker nodes on all ports. Added this to simplify the example and further avoid issues with Add-ons communication with Control plane.
-    # # This can be restricted further to specific port based on the requirement for each Add-on e.g., metrics-server 4443, spark-operator 8080, karpenter 8443 etc.
-    # # Change this according to your security requirements if needed
-    # ingress_cluster_to_node_all_traffic = {
-    #   description                   = "Cluster API to Nodegroup all traffic"
-    #   protocol                      = "-1"
-    #   from_port                     = 0
-    #   to_port                       = 0
-    #   type                          = "ingress"
-    #   source_cluster_security_group = true
-    # }
+    # Allows Control Plane Nodes to talk to Worker nodes on all ports. Added this to simplify the example and further avoid issues with Add-ons communication with Control plane.
+    # This can be restricted further to specific port based on the requirement for each Add-on e.g., metrics-server 4443, spark-operator 8080, karpenter 8443 etc.
+    # Change this according to your security requirements if needed
+    ingress_cluster_to_node_all_traffic = {
+      description                   = "Cluster API to Nodegroup all traffic"
+      protocol                      = "-1"
+      from_port                     = 0
+      to_port                       = 0
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
     egress_all = {
       description      = "Node all egress"
       protocol         = "-1"
@@ -65,19 +65,46 @@ module "eks" {
   }
 
   eks_managed_node_group_defaults = {
+
     iam_role_additional_policies = {
       # Not required, but used in the example to access the nodes to inspect mounted volumes
       AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     }
+
+    # NVMe instance store volumes are automatically enumerated and assigned a device
+    pre_bootstrap_user_data = <<-EOT
+      cat <<-EOF > /etc/profile.d/bootstrap.sh
+      #!/bin/sh
+
+      # Configure NVMe volumes in RAID0 configuration
+      # https://github.com/awslabs/amazon-eks-ami/blob/056e31f8c7477e893424abce468cb32bbcd1f079/files/bootstrap.sh#L35C121-L35C126
+      # Mount will be: /mnt/k8s-disks
+      export LOCAL_DISKS='raid0'
+      EOF
+
+      # Source extra environment variables in bootstrap script
+      sed -i '/^set -o errexit/a\\nsource /etc/profile.d/bootstrap.sh' /etc/eks/bootstrap.sh
+    EOT
+
+    ebs_optimized = true
+    # This bloc device is used only for root volume. Adjust volume according to your size.
+    # NOTE: Don't use this volume for Spark workloads
+    block_device_mappings = {
+      xvda = {
+        device_name = "/dev/xvda"
+        ebs = {
+          volume_size = 100
+          volume_type = "gp3"
+        }
+      }
+    }
   }
 
   eks_managed_node_groups = {
-    #  We recommend to have a MNG to place your critical workloads and add-ons
-    #  Then rely on Karpenter to scale your workloads
-    #  You can also make uses on nodeSelector and Taints/tolerations to spread workloads on MNG or Karpenter provisioners
-    doeks_node_group = {
-      name        = "doeks-node-group"
+    core_node_group = {
+      name        = "core-node-group"
       description = "EKS managed node group example launch template"
+      # Filtering only Secondary CIDR private subnets starting with "100.". Subnet IDs where the nodes/node groups will be provisioned
 
       ami_id = data.aws_ami.eks.image_id
       # This will ensure the bootstrap user data is used to join the node
@@ -100,30 +127,16 @@ module "eks" {
 
       min_size     = 1
       max_size     = 9
-      desired_size = var.node_group_desired_size
+      desired_size = var.core_node_group_desired_size
 
       force_update_version = true
       instance_types       = ["m5.xlarge"]
 
-      ebs_optimized = true
-      block_device_mappings = {
-        xvda = {
-          device_name = "/dev/xvda"
-          ebs = {
-            volume_size = 100
-            volume_type = "gp3"
-          }
-        }
-      }
-
       labels = {
         WorkerType    = "ON_DEMAND"
-        NodeGroupType = "doeks"
+        NodeGroupType = "core"
       }
 
-      tags = {
-        Name = "doeks-node-grp"
-      }
     }
   }
 
